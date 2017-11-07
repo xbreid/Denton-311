97a7f4ddb3b69657cb73cfd240dfa6ea923e91cb
Brandon Reid <brandonreid2@my.unt.edu>
2017-11-07 11:53:34 -0600

    added new app js, and package.json

--- App.js
+++ App.js
@@ -1,23 +1,57 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, View, ScrollView, TouchableOpacity } from 'react-native';
+import { ScreenOrientation } from 'expo';
+import { SafeAreaView, StackNavigator } from 'react-navigation';
+
+ScreenOrientation.allow(ScreenOrientation.Orientation.ALL);
+
+import MainScreen from './src/screens/MainScreen';
+import NewRequest from './src/screens/NewRequest';
+import RecentRequests from './src/screens/RecentRequests';
+import UserRequests from './src/screens/UserRequests';
+import UserInfo from './src/screens/UserInfo';
+
+const Routes = {
+  MainScreen: {
+    name: 'Welcome Screen',
+    description: 'Welcome splash screen',
+    screen: MainScreen,
+  },
+  NewRequest: {
+    name: 'New Request',
+    description: 'Create new non-emergency request',
+    screen: NewRequest,
+  },
+  RecentRequests: {
+    name: 'Recent Requests',
+    description: 'Recent requests in the city',
+    screen: RecentRequests,
+  },
+  UserRequests: {
+    name: 'My Requests',
+    description: 'Users recent requests',
+    screen: UserRequests,
+  },
+  UserInfo: {
+    name: 'My Info',
+    description: 'Application Settings/User Info',
+    screen: UserInfo,
+  },
+};
+
+const AppNavigator = StackNavigator(
+  {
+    ...Routes,
+  },
+  {
+    initialRouteName: 'MainScreen',
+    headerMode: 'none',
+    //mode: Platform.OS === 'ios' ? 'modal' : 'card',
+  },
+);
 
 export default class App extends React.Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
+    return <AppNavigator />;
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
