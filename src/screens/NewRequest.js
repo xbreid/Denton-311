19d92908030085b6a4f12e6e0711987f12d3e73a
Brandon Reid <brandonreid2@my.unt.edu>
2017-11-11 16:04:17 -0600

    added more issues to the list, 15 total now

--- src/screens/NewRequest.js
+++ src/screens/NewRequest.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Platform, StyleSheet, Text, View, ScrollView, TouchableOpacity, Button, Image} from 'react-native';
+import { Platform, StyleSheet, Text, View,
+  ScrollView, TouchableOpacity, Button, Image} from 'react-native';
 import { ScreenOrientation } from 'expo';
 import { SafeAreaView, StackNavigator, NavigationActions } from 'react-navigation';
 import Icon from 'react-native-vector-icons/Ionicons';
@@ -9,6 +10,42 @@ const testScreen = ({ navigation }) => (
     <Button onPress={() => navigation.goBack(null)} title="Go back" />
   </SafeAreaView>
 );
+
+const Animals = {
+  DeadAnimal: {
+    name: 'Dead Animal',
+    image: require('../../assets/images/cow-skull.png'),
+    screen: testScreen,
+    navigationOptions: ({navigation}) => ({
+      title: 'Dead Animal',
+      headerLeft: (headerBack(navigation)),
+      headerStyle: styles.header,
+      headerTitleStyle: styles.headerTitle
+    }),
+  },
+  FoundAnimal: {
+    name: 'Found Animal',
+    image: require('../../assets/images/found-animal.png'),
+    screen: testScreen,
+    navigationOptions: ({navigation}) => ({
+      title: 'Found Animal',
+      headerLeft: (headerBack(navigation)),
+      headerStyle: styles.header,
+      headerTitleStyle: styles.headerTitle
+    }),
+  },
+  LooseAnimal: {
+    name: 'Loose Animal',
+    image: require('../../assets/images/running-dog-silhouette.png'),
+    screen: testScreen,
+    navigationOptions: ({navigation}) => ({
+      title: 'Loose Animal',
+      headerLeft: (headerBack(navigation)),
+      headerStyle: styles.header,
+      headerTitleStyle: styles.headerTitle
+    }),
+  },
+};
 
 const StreetsSidewalks = {
   Pothole: {
@@ -94,7 +131,7 @@ const ParkingVehicles = {
   },
   BlockedDriveway: {
     name: 'Blocked Driveway',
-    image: require('../../assets/images/blocked.png'),
+    image: require('../../assets/images/blocked-sign.png'),
     screen: testScreen,
     navigationOptions: ({navigation}) => ({
       title: 'Blocked Driveway',
@@ -103,27 +140,75 @@ const ParkingVehicles = {
       headerTitleStyle: styles.headerTitle
     }),
   },
+  IllegalParking: {
+    name: 'Illegal Parking',
+    image: require('../../assets/images/no-parking-sign.png'),
+    screen: testScreen,
+    navigationOptions: ({navigation}) => ({
+      title: 'Illegal Parking',
+      headerLeft: (headerBack(navigation)),
+      headerStyle: styles.header,
+      headerTitleStyle: styles.headerTitle
+    }),
+  },
+  ParkingMeter: {
+    name: 'Parking Meter',
+    image: require('../../assets/images/parking-meter.png'),
+    screen: testScreen,
+    navigationOptions: ({navigation}) => ({
+      title: 'Parking Meter',
+      headerLeft: (headerBack(navigation)),
+      headerStyle: styles.header,
+      headerTitleStyle: styles.headerTitle
+    }),
+  },
+};
+
+const Sanitation = {
+  IllegalDumping: {
+    name: 'Illegal Dumping',
+    image: require('../../assets/images/trash.png'),
+    screen: testScreen,
+    navigationOptions: ({navigation}) => ({
+      title: 'Illegal Dumping',
+      headerLeft: (headerBack(navigation)),
+      headerStyle: styles.header,
+      headerTitleStyle: styles.headerTitle
+    }),
+  },
+  BuildingGraffiti: {
+    name: 'Building Graffiti',
+    image: require('../../assets/images/spray.png'),
+    screen: testScreen,
+    navigationOptions: ({navigation}) => ({
+      title: 'Building Graffiti',
+      headerLeft: (headerBack(navigation)),
+      headerStyle: styles.header,
+      headerTitleStyle: styles.headerTitle
+    }),
+  },
 };
 
 
-const IssueButton = (navigation, issueCat, issueRoute, i, arr) => (
+// Issue button component
+const IssueButton = (navigation, issueCat, issue, issueIndex, issueArr) => (
   <TouchableOpacity
     key={issueCat}
     onPress={() => {
-      const { path, params, screen } = issueRoute;
+      const { path, params, screen } = issue;
       const { router } = screen;
       const action = path && router.getActionForPathAndParams(path, params);
       navigation.navigate(issueCat, {}, action);
     }}
   >
     <SafeAreaView
-      style={arr.length - 1 === i ? '' : styles.itemBorder}
+      style={issueArr.length - 1 === issueIndex ? '' : styles.itemBorder}
       forceInset={{ vertical: 'never' }}
     >
-      <Image style={{height: 50, width: 50, margin: 10,  position: 'absolute' }} source={issueRoute.image}/>
+      <Image style={styles.issueIcon} source={issue.image}/>
       <View style={styles.item}>
         <Text style={styles.title}>
-          {issueRoute.name}
+          {issue.name}
         </Text>
         <Icon name="ios-arrow-forward" style={{paddingHorizontal: 3}} color="#BDBDBD" size={22}/>
       </View>
@@ -134,6 +219,12 @@ const IssueButton = (navigation, issueCat, issueRoute, i, arr) => (
 const RequestScreen = ({ navigation }) => (
   <ScrollView style={{ flex: 1 }} contentInsetAdjustmentBehavior="automatic">
       <View key="StreetsSidewalks">
+        <Text style={styles.categoryTitle}>
+          ANIMALS
+        </Text>
+        {Object.keys(Animals).map((issueCategory: string, i, arr) => (
+          IssueButton(navigation, issueCategory, Animals[issueCategory], i, arr)
+        ))}
         <Text style={styles.categoryTitle}>
           STREETS & SIDEWALKS
         </Text>
@@ -146,6 +237,12 @@ const RequestScreen = ({ navigation }) => (
         {Object.keys(ParkingVehicles).map((issueCategory: string, i, arr) => (
           IssueButton(navigation, issueCategory, ParkingVehicles[issueCategory], i, arr)
         ))}
+        <Text style={styles.categoryTitle}>
+          SANITATION
+        </Text>
+        {Object.keys(Sanitation).map((issueCategory: string, i, arr) => (
+          IssueButton(navigation, issueCategory, Sanitation[issueCategory], i, arr)
+        ))}
       </View>
   </ScrollView>
 );
@@ -156,11 +253,12 @@ const headerBack = (navigation) => (
   </TouchableOpacity>
 );
 
-
 const MainStack = StackNavigator(
   {
+    ...Animals,
     ...StreetsSidewalks,
     ...ParkingVehicles,
+    ...Sanitation,
     Index: {
       screen: RequestScreen,
       navigationOptions: ({navigation}) => ({
@@ -176,7 +274,6 @@ const MainStack = StackNavigator(
   }
 );
 
-
 const styles = StyleSheet.create({
   item: {
     paddingHorizontal: 20,
@@ -190,6 +287,12 @@ const styles = StyleSheet.create({
     borderBottomWidth: StyleSheet.hairlineWidth,
     borderColor: '#BDBDBD',
     borderBottomLeftRadius: 80
+  },
+  issueIcon: {
+    height: 50,
+    width: 50,
+    margin: 10,
+    position: 'absolute'
   },
   title: {
     fontSize: 20,
